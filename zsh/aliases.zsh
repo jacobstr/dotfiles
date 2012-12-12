3dc7deb2ff39d20352c714b647b882dbefdf09a0
jacobstr <jacobstr@gmail.com>
2012-12-12 08:43:33 -0700

    Making my customization. Mainly deleting things I dont need.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,21 @@
 alias reload!='. ~/.zshrc'
+
+alias dotrc='. ~/.zshrc'
+alias zshconfig="vim ~/.zshrc"
+alias zshenv="vim ~/.zshenv"
+
+# Zsh Directory Bookmarks
+alias m1='alias g1="cd `pwd`"'
+alias m2='alias g2="cd `pwd`"'
+alias m3='alias g3="cd `pwd`"'
+alias m4='alias g4="cd `pwd`"'
+alias m5='alias g5="cd `pwd`"'
+alias m6='alias g6="cd `pwd`"'
+alias m7='alias g7="cd `pwd`"'
+alias m8='alias g8="cd `pwd`"'
+alias m9='alias g9="cd `pwd`"'
+alias mdump='alias -L|grep -e "alias g[0-9]"|grep -v "alias m" > ~/.bookmarks'
+alias lma='alias -L|grep -e "alias g[0-9]"|grep -v "alias m"|sed "s/alias //"'
+touch ~/.bookmarks
+source ~/.bookmarks
+
